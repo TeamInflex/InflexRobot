3142e364b5678871ea70b46e4c478d45457c2bd4
Team Inflex <TheTeamInflex@Gmail.com>
2023-02-18 16:11:06 +0530

    Update config.py

--- InflexRobot/config.py
+++ InflexRobot/config.py
@@ -2,14 +2,14 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 6
+    API_ID = "1028282"
     API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
 
     CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
     DATABASE_URL = ""  # A sql database url from elephantsql.com
 
-    EVENT_LOGS = ()  # Event logs channel to note down important bot level events
+    EVENT_LOGS = "-100282992882"  # Event logs channel to note down important bot level events
 
     MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
 
@@ -18,11 +18,11 @@ class Config(object):
 
     SUPPORT_CHAT = "InflexSupport"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "7299383822:joininflexupdatesandsupport"  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 1356469075  # User id of your telegram account (Must be integer)
+    OWNER_ID = "1356469075"  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
